42078d3c6b6aa5fd9dae9f7592292da95e1110e4
Foxinat <vanlak21808@gmail.com>
2024-11-26 20:56:43 +0300

    практикум 10

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = Input("a = ");
+            int number2 = Input("b = ");
+            Console.WriteLine($"Наибольшее число = {Math.Max(number1, number2)}");
+            Console.ReadKey();
+        }
+
+        static int Input(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+
+        }
+    }
+}
